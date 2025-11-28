029383c641d7df3fdd66bb8b6b78168b89c7031b
Scott Cooper <Scott50000@gmail.com>
2025-11-28 01:05:26 -0500

    Build number 2

--- frontend/app.config.js
+++ frontend/app.config.js
@@ -18,7 +18,7 @@ export default ({ config }) => ({
       process.env.APP_VARIANT === "dev"
         ? "com.plasmadiffusion.stretchesdev"
         : "com.plasmadiffusion.stretches",
-    buildNumber: process.env.BUILD_NUMBER || "1",
+    buildNumber: "2",
   },
   android: {
     adaptiveIcon: {
