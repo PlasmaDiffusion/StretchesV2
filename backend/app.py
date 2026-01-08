d22c77d746cf2810b8d8da5786a32262230a552c
Scott Cooper <Scott50000@gmail.com>
2026-01-08 17:46:26 -0500

    physiotherapy_advice route

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask
+from flask import Flask, request
 from openai import OpenAI
 from dotenv import load_dotenv
 
@@ -17,7 +17,7 @@ client = OpenAI(
 def hello_world():
     return "<p>Hello, World!</p>"
 
-
+# Test route to verify AI model and key works
 @app.route("/ai-test")
 def ai_test():
     response = client.responses.create(
@@ -27,6 +27,28 @@ def ai_test():
     )
 
     return f"<p>{response.output_text}</p>"
+
+# Gets JSON body of prompt of something stretching, physiotherapy, or pain related, depending on response_type. Returns AI response.
+@app.route("/physiotherapy_advice", methods=['POST'])
+def physiotherapy_advice():
+    data = request.get_json()  
+    message = data.get('message', '')
+    adviceType = data.get('advice_type', 'stretches')
+    
+    instructions_map = {
+        'stretches': "You are a specialized Physiotherapy Assistant. Your goal is to provide evidence-based pain management education. Recommend stretches and exercises based on user input.",
+        'mental': "You are a specialized Physiotherapy Assistant. Your goal is to provide evidence-based pain management education. Recommend ways to cope with the pain mentally.",
+        'physiotherapy': "You are a specialized Physiotherapy Assistant. Your goal is to provide evidence-based pain management education. Talk about what physiotherapists could do aside from assigning you stretches based on user input.", 
+
+    }
+    
+    response = client.responses.create(
+        model="gpt-5-nano",
+        instructions=instructions_map.get(adviceType, instructions_map['stretches']),
+        input=message,
+    )
+    
+    return {"response": response.output_text}
 
 
 if __name__ == '__main__':
