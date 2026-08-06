92cd2c4777decb6df813d5448c8b34ffdd76240f
Scott Cooper <Scott50000@gmail.com>
2026-08-06 17:40:29 -0400

    Build version 6 - Fixes for Advice responses and UX tweaks

--- frontend/app.config.js
+++ frontend/app.config.js
@@ -18,7 +18,7 @@ export default ({ config }) => ({
       process.env.APP_VARIANT === "dev"
         ? "com.plasmadiffusion.stretchesdev"
         : "com.plasmadiffusion.stretches",
-    buildNumber: "5",
+    buildNumber: "6",
   },
   android: {
     adaptiveIcon: {
